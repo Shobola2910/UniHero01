d7cbe81e6e6e326079f6c84100d3e21c8fed28c6
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-22 13:54:41 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+
+export default function HomePage() {
+  return (
+    <div className="space-y-12">
+      <Hero />
+      {/* ...more sections */}
+    </div>
+  );
+}
