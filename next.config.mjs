e82b8dbbad20d12dd29c5ed27852d9847e293b94
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-22 13:55:49 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // If you keep remote images, add them here
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdn.yoursite.com" },
+    ],
+  },
 };
+
 export default nextConfig;
