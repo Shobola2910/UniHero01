90cee831c5cde15fa92b03a2c6c6d4c789d0d40d
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-22 08:31:09 -0400

    Create SiteFooter.tsx

---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,8 @@
+// src/components/SiteFooter.tsx
+export default function SiteFooter() {
+  return (
+    <footer className="py-6 text-center text-sm opacity-70">
+      © {new Date().getFullYear()} UniHero
+    </footer>
+  );
+}
