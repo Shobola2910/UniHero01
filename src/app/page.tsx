ca4b5be5750fde1a6777ab5ec6252fedddeda848
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-22 14:51:25 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <div className="space-y-12">
+    <>
       <Hero />
-      {/* ...more sections */}
-    </div>
+      {/* keyinroq: features, stats va h.k. */}
+    </>
   );
 }
