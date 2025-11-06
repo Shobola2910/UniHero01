112d7851980a8d61081053de37d661cb9962c2a2
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-06 18:02:29 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import Hero from "@/components/Hero";
-import WhyLove from "@/components/WhyLove";
+// src/app/page.tsx
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Resources from "../components/Resources";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer"; // Agar sizda SiteFooter bor bo'lsa, shuni import qiling
 
-export default function HomePage() {
+export default function Page() {
   return (
     <>
-      <Hero />
-      <WhyLove />
+      <Navbar />
+      <main>
+        <Hero />        {/* #home */}
+        <Features />    {/* #about */}
+        <Resources />   {/* #resources */}
+        <Contact />     {/* #contact */}
+      </main>
+      <Footer />
     </>
   );
 }
