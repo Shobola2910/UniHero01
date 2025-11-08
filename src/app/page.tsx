76f81f0636b0e0253a7fb4f224b9da9e74aed68d
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-07 22:54:41 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 // src/app/page.tsx
-import Navbar from "@/components/Navbar";
-import AboutSection from "@/components/about/AboutSection";
-import ResourcesSection from "@/components/sections/Resources";
-// Agar Contact bo‘limingiz bo‘lmasa, importni va JSXni olib tashlang yoki keyin qo‘shasiz:
-import ContactSection from "@/components/ContactSection";
+import Navbar from "../components/Navbar";
+// Alias bilan muammo bo‘lmasligi uchun RELATIV import:
+import AboutSection from "../components/about/AboutSection";
+import ResourcesSection from "../components/sections/Resources";
+// Agar Contact bor bo‘lsa shu importni qoldiring, bo‘lmasa olib tashlang:
+import ContactSection from "../components/ContactSection";
 
 export default function HomePage() {
   return (
     <main>
       <Navbar />
 
-      {/* Hero bo‘limingiz alohida bo‘lsa, shu yerga qo‘ying (ixtiyoriy) */}
+      {/* Hero bo‘limingiz bo‘lmasa, tashlab ketavering */}
       {/* <Hero /> */}
 
       <section id="about">
